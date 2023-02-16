49dbf132730eab799a6cd098c5db4e9d8414dd65
Cozdan <kolya2k@bk.ru>
2023-02-16 20:02:37 +0300

    SearchServer [Paginator]

--- main.cpp
+++ main.cpp
@@ -8,29 +8,20 @@
 
 using namespace std;
 
-template <typename It>
-void PrintRange(It range_begin, It range_end) {
-    bool is_first = 1;
-    for (auto it = range_begin; it != range_end; ++it) {
-        if (!is_first) {
-            cout << " ";
-        }
-        cout << *it;
-        is_first = 0;
-    }
-    if (!is_first) {
-        cout << endl;
-    }
+
+// разработайте сигнатуру фунции MakeVector по аналогии с функцией MakeSet из урока
 
 
+
+uint64_t Factorial(int num) {
+    int factorial = 1;
+    cout << "Текущее число = "s << num << endl;
+    if (num != 0) {
+        factorial = Factorial(num - 1) * num;
+    }
+    cout << "Значение факториала "s << factorial << "для числа "s << num << endl;
+    return factorial;
 }
-
 int main() {
-    cout << "Test1"s << endl;
-    set<int> test1 = { 1, 1, 1, 2, 3, 4, 5, 5 };
-    PrintRange(test1.begin(), test1.end());
-    cout << "Test2"s << endl;
-    vector<int> test2 = {}; // пустой контейнер
-    PrintRange(test2.begin(), test2.end());
-    cout << "End of tests"s << endl;
+    Factorial(4);
 }
